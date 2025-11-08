4e4738f83dbcdfc24a9795f0cbb9a80817dad5be
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-11-08 11:55:59 +0900

    replace latest kernel to zen

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -4,36 +4,36 @@
   # Bootloader configuration
   boot = {
     # Use latest kernel
-    kernelPackages = pkgs.linuxPackages_latest;
-    
+    kernelPackages = pkgs.linuxPackages_zen;
+
     # Kernel parameters
     kernelParams = [
       "quiet"
       "splash"
     ];
-    
+
     # Boot loader
     loader = {
       systemd-boot = {
         enable = true;
         configurationLimit = 5;
         consoleMode = "auto";
-        editor = false;  # Disable editor for security
+        editor = false; # Disable editor for security
       };
-      
+
       efi = {
         canTouchEfiVariables = true;
         efiSysMountPoint = "/boot";
       };
-      
+
       timeout = 3;
     };
-    
+
     # Plymouth for boot splash (optional)
     plymouth = {
-      enable = false;  # Set to true if you want boot splash
+      enable = false; # Set to true if you want boot splash
     };
-    
+
     # Tmp on tmpfs for better performance
     tmp = {
       useTmpfs = true;
