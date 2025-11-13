bc36f29a292c998701fa27a084c2c8c34455c1ea
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-11-14 07:31:14 +0900

    change swaybg option

--- modules/home/wayland/niri/startups.nix
+++ modules/home/wayland/niri/startups.nix
@@ -5,6 +5,6 @@
     { command = [ "xwayland-satellite" ]; } # XWayland support for X11 apps like Steam
     { command = [ "waybar" ]; }
     { command = [ "mako" ]; }
-    { command = [ "swaybg" "-i" vars.wallpaperPath "-m" "fit" ]; }
+    { command = [ "swaybg" "-i" vars.wallpaperPath "-m" "center" ]; }
   ];
 }
