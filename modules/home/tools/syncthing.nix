a7bd0003d2573fd3c0c5ee8a8bab580e633fe4fa
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-07 22:24:20 +0900

    add syncthing id

--- modules/home/tools/syncthing.nix
+++ modules/home/tools/syncthing.nix
@@ -3,7 +3,7 @@ let
   deviceList =
     {
       "desuwa" = { id = "ZR7INJC-T33D5ZE-SU6AAXE-ZNQ47VD-IWMU5MG-HHXU5N3-ITJKIOV-WRAYBAD"; };
-      "nanodesu" = { id = ""; };
+      "nanodesu" = { id = "IWWSRPT-PXMX62T-UYRIGNA-F3RQNFD-ZQKSOMP-2GVEK2K-7VLVGO3-QL434AK"; };
       "phone" = { id = "Y34UUSW-2VJSWWW-U7HDZYV-W62BLJB-LMJHF66-B27X2A7-LFZS27Q-TTU5ZQB"; };
     };
 
