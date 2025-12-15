591089ded7fb35b7be8c6819b9a70df1e9dfe999
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-15 15:35:10 +0900

    add juice-daemon

--- home/nanodesu/home.nix
+++ home/nanodesu/home.nix
@@ -1,5 +1,8 @@
-{ pkgs, vars, ... }:
-
+{ vars, pkgs, inputs, ... }:
+let
+  juice_path =
+    inputs.juice.packages.${pkgs.stdenv.hostPlatform.system}.default;
+in
 {
   # Import common home configuration
   imports = [
@@ -17,5 +20,24 @@
     zoom-us
 
     libreoffice-qt
+
+    juice_path
   ];
+
+  systemd.user.services.juice-daemon = {
+    Unit = {
+      Description = "Juice battery history daemon";
+      After = [ "default.target" ];
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${juice_path}/bin/juice daemon";
+      # ExecStart = "%h/.cargo/bin/juice daemon";
+      Restart = "on-failure";
+      RestartSec = 30;
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
