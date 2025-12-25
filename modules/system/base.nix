659672be98a644902571b0f8f0bc6f242b373f97
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-25 23:29:27 +0900

    remove cachix urls

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -30,16 +30,6 @@
       experimental-features = [ "nix-command" "flakes" ];
       auto-optimise-store = true;
       trusted-users = [ "root" vars.username ];
-      substituters = [
-        "https://cache.nixos.org"
-        "https://nix-community.cachix.org"
-        "https://vicinae.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-      ];
     };
 
     # gc = {
