040deb56b03121e1da2a3117a190b82459a818c3
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-26 00:09:44 +0900

    add search config to zen-browser

--- modules/home/browser/zen-browser.nix
+++ modules/home/browser/zen-browser.nix
@@ -35,6 +35,47 @@
         darkreader
         proton-pass
       ];
+
+      settings = { };
+
+      search = {
+        force = true;
+        default = "google";
+        engines = {
+          mynixos = {
+            name = "My NixOS";
+            urls = [
+              {
+                template = "https://mynixos.com/search?q={searchTerms}";
+                params = [
+                  {
+                    name = "query";
+                    value = "searchTerms";
+                  }
+                ];
+              }
+            ];
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nx" ]; # Keep in mind that aliases defined here only work if they start with "@"
+          };
+
+          youtube = {
+            name = "Youtube";
+            urls = [
+              {
+                template = "https://www.youtube.com/results?search_query={BsearchTerms}";
+                params = [
+                  {
+                    name = "query";
+                    value = "searchTerms";
+                  }
+                ];
+              }
+            ];
+            definedAliases = [ ";" ];
+          };
+        };
+      };
     };
   };
 }
