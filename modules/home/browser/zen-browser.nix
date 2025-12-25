a44c616d3d0e978fe65e4a2a8def23f7018b49f9
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-25 23:30:05 +0900

    add policies and extensions to zen-browser

--- modules/home/browser/zen-browser.nix
+++ modules/home/browser/zen-browser.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
@@ -7,5 +7,34 @@
 
   programs.zen-browser = {
     enable = true;
+
+    policies = {
+      AutofillAddressEnabled = false;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+
+    profiles.default = {
+      extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
+        ublock-origin
+        privacy-badger
+        auto-tab-discard
+        darkreader
+        proton-pass
+      ];
+    };
   };
 }
