84f6a1efec549da47b5bd3948bd43b3fc3cf0a51
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-29 00:22:09 +0900

    remove unused args

--- modules/home/wayland/mako.nix
+++ modules/home/wayland/mako.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, uiSettings, ... }:
+{ pkgs, uiSettings, ... }:
 
 {
   services.mako = {
--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # Wayland environment variables
