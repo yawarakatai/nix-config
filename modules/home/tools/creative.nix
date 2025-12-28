96c98c0a9bad0b4b131154b5419d44b947a80618
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-28 15:30:22 +0900

    add bambu-studio

--- modules/home/tools/creative.nix
+++ modules/home/tools/creative.nix
@@ -1,8 +1,30 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
+    # Game Development
     godot
+
+    # PCB design
     kicad
+
+    # 3D printing
+    bambu-studio
+
+    # Video editing
+    davinci-resolve
+    (writeShellScriptBin "resolve" ''
+      export DISPLAY=:0
+      export QT_QPA_PLATFORM=xcb
+      exec davinci-resolve "$@"
+    '')
   ];
+
+  xdg.desktopEntries.davinci-resolve = {
+    name = "DaVinci Resolve";
+    exec = "env DISPLAY=:0 QT_QPA_PLATFORM=xcb davinci-resolve %U";
+    icon = "DV_Resolve";
+    terminal = false;
+    categories = [ "AudioVideo" "Video" ];
+  };
 }
