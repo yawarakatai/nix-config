961a12d94de85e51b7f9b7a134ce62500edf7012
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-28 15:30:13 +0900

    commentout brave

--- home/desuwa/home.nix
+++ home/desuwa/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, vars, ... }:
+{ vars, ... }:
 
 {
   imports = [
@@ -14,29 +14,12 @@
   home.username = vars.username;
   home.homeDirectory = "/home/${vars.username}";
 
-  home.packages = with pkgs; [
-    davinci-resolve
-    (writeShellScriptBin "resolve" ''
-      export DISPLAY=:0
-      export QT_QPA_PLATFORM=xcb
-      exec davinci-resolve "$@"
-    '')
-  ];
-
-  xdg.desktopEntries.davinci-resolve = {
-    name = "DaVinci Resolve";
-    exec = "env DISPLAY=:0 QT_QPA_PLATFORM=xcb davinci-resolve %U";
-    icon = "DV_Resolve";
-    terminal = false;
-    categories = [ "AudioVideo" "Video" ];
-  };
-
-  programs.brave = {
-    enable = true;
-    commandLineArgs = [
-      "--enable-features=UseOzonePlatform,VaapiVideoDecoder,VaapiVideoEncoder"
-      "--ozone-platform=wayland"
-      "--enable-zero-copy"
-    ];
-  };
+  # programs.brave = {
+  #   enable = true;
+  #   commandLineArgs = [
+  #     "--enable-features=UseOzonePlatform,VaapiVideoDecoder,VaapiVideoEncoder"
+  #     "--ozone-platform=wayland"
+  #     "--enable-zero-copy"
+  #   ];
+  # };
 }
