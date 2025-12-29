e14f99c49ff7571b0cb77ca796fc6de4db08a9c5
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2025-12-29 22:21:15 +0900

    add ssh public keys

--- modules/home/service/ssh.nix
+++ modules/home/service/ssh.nix
@@ -1,14 +1,23 @@
 { ... }:
 let
+  yubikeyKeys = {
+    yubikey_5 = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKoUC9mEqLf9q8geELb89t8I9P+0JBD2fvm51+jwNuu3AAAABHNzaDo= yubikey_5";
+    yubikey_5c = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIFwdOpc6zvMiZ0zC/NqC2mzEn0B5hdRz1jD2V76vsclLAAAABHNzaDo= yubikey_5c";
+  };
+
   yubikeyIdentities = [
     "~/.ssh/yubikey_5"
     "~/.ssh/yubikey_5c"
   ];
 in
 {
+  home.file = {
+    ".ssh/yubikey_5.pub".text = yubikeyKeys.yubikey_5;
+    ".ssh/yubikey_5c.pub".text = yubikeyKeys.yubikey_5c;
+  };
+
   programs.ssh = {
     enable = true;
-
     enableDefaultConfig = false;
 
     matchBlocks = {
@@ -25,7 +34,6 @@ in
       };
 
       "192.168.*" = {
-        # forwardAgent = true;
         identityFile = yubikeyIdentities;
         identitiesOnly = true;
       };
