418c94104b05c7c313dcb24c982593ff358fef63
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-01-04 15:48:23 +0900

    include master key to secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,7 @@
 let
   keys = import ./keys.nix;
-  allKeys = (builtins.attrValues keys.yubikey) ++ (builtins.attrValues keys.hosts);
+  masterKey = "age1xnx786rjc69fp65qqruglkqy8tlgjl3x7akvrlke4h3cpprsx9gsm88het";
+  allKeys = (builtins.attrValues keys.yubikey) ++ (builtins.attrValues keys.hosts) ++ [ masterKey ];
 in
 {
   "user-password.age".publicKeys = allKeys;
