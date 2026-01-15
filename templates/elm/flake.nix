f2f312a889929da5fe133edc80fc04fc00cabaf1
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-01-15 15:21:46 +0900

    format elm/flake.nix

--- templates/elm/flake.nix
+++ templates/elm/flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
@@ -20,10 +26,10 @@
             elmPackages.elm-test
             elmPackages.elm-review
             elmPackages.elm-json
-            
+
             # Development server for live reload
             elmPackages.elm-live
-            
+
             # Optional: Node.js for additional tooling
             nodejs
           ];
