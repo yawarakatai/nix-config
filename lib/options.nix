9e400b7e2fab788979654fd994bab3275df54867
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-01-18 08:16:37 +0900

    feat: add option

---
+++ lib/options.nix
@@ -0,0 +1,45 @@
+{ lib, ... }:
+with lib;
+{
+  options.my = {
+    # ユーザー名
+    user.name = mkOption {
+      type = types.str;
+      default = "yawarakatai";
+    };
+
+    system = {
+      monitors = mkOption {
+        type = types.attrsOf (
+          types.submodule {
+            options = {
+              name = mkOption { type = types.str; };
+              width = mkOption { type = types.int; };
+              height = mkOption { type = types.int; };
+              refresh = mkOption { type = types.float; };
+              scale = mkOption {
+                type = types.float;
+                default = 1.0;
+              };
+              vrr = mkOption {
+                type = types.bool;
+                default = false;
+              };
+              position = {
+                x = mkOption {
+                  type = types.int;
+                  default = 0;
+                };
+                y = mkOption {
+                  type = types.int;
+                  default = 0;
+                };
+              };
+            };
+          }
+        );
+        default = { };
+      };
+    };
+  };
+}
