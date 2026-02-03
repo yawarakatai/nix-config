443d142e839a70d6ecd442dec095cc581b4fed29
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-03 12:39:52 +0900

    feat: add python/flake.nix

---
+++ templates/python/flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              python3
+              uv
+              pyright
+            ];
+
+            shellHook = ''
+              if [ ! -d ".venv" ]; then
+                uv venv
+              fi
+              source .venv/bin/activate
+            '';
+          };
+        };
+    };
+}
