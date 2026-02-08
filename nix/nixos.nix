1344ccd3ac9b47b20b1c1601df615e8c84c832c4
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-08 21:26:48 +0900

    feat: remove lib.nix and update nixos.nix

--- nix/nixos.nix
+++ nix/nixos.nix
@@ -4,56 +4,122 @@
   ...
 }:
 let
-  lib = import ./lib.nix { inherit inputs; };
+  mkSystem =
+    {
+      hostname,
+      system,
+      extraModules ? [ ],
+    }:
+    inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
 
-  commonModules = [
-    inputs.agenix.nixosModules.default
-    inputs.agenix-rekey.nixosModules.default
-    inputs.home-manager.nixosModules.home-manager
-    ../modules/system
+      specialArgs = { inherit inputs; };
+
+      modules = [
+        {
+          nixpkgs.overlays = import ../overlays;
+          networking.hostName = hostname;
+          nixpkgs.hostPlatform = system;
+          nixpkgs.config.allowUnfree = true;
+        }
+
+        inputs.home-manager.nixosModules.home-manager
+        ../hosts/${hostname}
+
+        (
+          { config, ... }:
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.${config.my.user.name} = import ../home/${hostname}/home.nix;
+              extraSpecialArgs = { inherit inputs; };
+              backupFileExtension = "backup";
+            };
+          }
+        )
+      ]
+      ++ extraModules;
+    };
+
+  mkHome =
+    {
+      username,
+      hostname,
+      system,
+    }:
+    inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      extraSpecialArgs = { inherit inputs; };
+      modules = [
+        ../home/${hostname}/home.nix
+      ];
+    };
+
+  baseModules = [
+    ../modules/system/core/networking.nix
+    ../modules/system/core/nix.nix
+    ../modules/system/storage/zram.nix
+    ../modules/system/service/tailscale.nix
   ];
 
-  desktopModules = commonModules ++ [
+  secretModules = [
+    inputs.agenix.nixosModules.default
+    inputs.agenix-rekey.nixosModules.default
+    ../modules/system/core/users.nix
+    ../modules/system/security
+    ../modules/system/service/openssh.nix
+  ];
+
+  desktopModules = [
     inputs.stylix.nixosModules.stylix
     inputs.niri.nixosModules.niri
+    ../modules/system/core/boot.nix
     ../modules/system/desktop
     ../modules/system/display
     ../modules/theme
     ../modules/system/hardware/audio.nix
+    ../modules/system/storage/btrfs.nix
   ];
 in
 {
   flake = {
     nixosConfigurations = {
-      desuwa = lib.mkSystem {
+      desuwa = mkSystem {
         hostname = "desuwa";
         system = "x86_64-linux";
-        extraModules = desktopModules;
+        extraModules = baseModules ++ secretModules ++ desktopModules;
       };
 
-      nanodesu = lib.mkSystem {
+      nanodesu = mkSystem {
         hostname = "nanodesu";
         system = "x86_64-linux";
-        extraModules = desktopModules;
+        extraModules = baseModules ++ secretModules ++ desktopModules;
+      };
+
+      daze = mkSystem {
+        hostname = "daze";
+        system = "aarch64-linux";
+        extraModules = baseModules;
       };
     };
 
     homeConfigurations = {
-      "yawarakatai@dayo" = inputs.home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs {
-          system = "aarch64-linux";
-          config.allowUnfree = true;
-        };
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ../home/dayo/home.nix
-        ];
+      "yawarakatai@dayo" = mkHome {
+        username = "yawarakatai";
+        hostname = "dayo";
+        system = "aarch64-linux";
       };
     };
 
     agenix-rekey = inputs.agenix-rekey.configure {
       userFlake = self;
-      nixosConfigurations = self.nixosConfigurations;
+      nixosConfigurations = {
+        inherit (self.nixosConfigurations) desuwa nanodesu;
+      };
     };
   };
 }
