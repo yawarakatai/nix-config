1574063c205889a9bac013bc2a255d84f3572127
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-08 21:50:05 +0900

    feat: update home.nix

--- home/daze/home.nix
+++ home/daze/home.nix
@@ -1,8 +1,18 @@
 { osConfig, ... }:
 
 {
+  imports = [
+    ../../modules/home/cli/core.nix
+    ../../modules/home/cli/monitor.nix
+    ../../modules/home/editor/helix.nix
+    ../../modules/home/dev/git.nix
+    ../../modules/home/dev/direnv.nix
+  ];
+
   home.username = osConfig.my.user.name;
   home.homeDirectory = "/home/${osConfig.my.user.name}";
+
+  programs.home-manager.enable = true;
 
   home.stateVersion = "25.05";
 }
