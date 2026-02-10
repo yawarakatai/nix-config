479197a5e0f16a6d0d6722db6aa57bfec66f4df4
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-10 12:09:05 +0900

    feat: create config file for home-assistant

--- modules/system/server/containers/home-assistant.nix
+++ modules/system/server/containers/home-assistant.nix
@@ -1,5 +1,21 @@
 { ... }:
 
+let
+  haConfig = ''
+    homeassistant:
+      name: Home
+      unit_system: metric
+      time_zone: Asia/Tokyo
+
+    default_config:
+
+    http:
+      use_x_forwarded_for: true
+      trusted_proxies:
+        - 127.0.0.1
+        - ::1
+  '';
+in
 {
   virtualisation.oci-containers.backend = "docker";
 
@@ -11,9 +27,31 @@
     ];
     extraOptions = [
       "--network=host"
+      "--cap-add=NET_ADMIN"
+      "--cap-add=NET_RAW"
     ];
     environment = {
       TZ = "Asia/Tokyo";
     };
+  };
+
+  systemd.services.homeassistant-config = {
+    description = "Setup Home Assistant configuration";
+    before = [ "docker-homeassistant.service" ];
+    wantedBy = [ "docker-homeassistant.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      mkdir -p /var/lib/homeassistant
+
+      # create configuration.yaml at the first time
+      if [ ! -f /var/lib/homeassistant/configuration.yaml ]; then
+        cat > /var/lib/homeassistant/configuration.yaml << 'HACONFIG'
+      ${haConfig}
+      HACONFIG
+      fi
+    '';
   };
 }
