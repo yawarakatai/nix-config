70463e4689aa23df6c55dbaaaeef7ed6c19e9cb9
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-10 13:04:04 +0900

    feat: update secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,4 +6,7 @@ in
     (builtins.attrValues keys.yubikey)
     ++ (builtins.attrValues keys.hosts)
     ++ (builtins.attrValues keys.master);
+
+  "ha-token.age".publicKeys =
+    (builtins.attrValues keys.yubikey) ++ [ keys.hosts.daze ] ++ (builtins.attrValues keys.master);
 }
