4463d814e9ec1805456e2c5a94902b92914fca1f
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-18 21:16:53 +0900

    feat: add new machine desuno

---
+++ hosts/desuno/default.nix
@@ -0,0 +1,40 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../modules/system/gaming # Steam
+    ../../modules/system/service/udev.nix # Udev
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Tmp on tmpfs for better performance
+  boot.tmp = {
+    useTmpfs = true;
+    tmpfsSize = "50%";
+  };
+
+  # --- Application and Service Settings ---;
+  environment.sessionVariables = {
+    __GL_SHADER_DISK_CACHE = "1";
+    __GL_THREADED_OPTIMIZATION = "1";
+  };
+
+  # --- My Options ---
+
+  my = {
+    system.monitors.primary = {
+      name = "DP-1";
+      width = 3840;
+      height = 2160;
+      refresh = 59.940;
+      vrr = true;
+    };
+  };
+
+  # --- State Version ---
+  # State version - DO NOT CHANGE after initial install
+  system.stateVersion = "25.05";
+}
