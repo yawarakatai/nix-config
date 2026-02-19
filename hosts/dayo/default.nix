2f9bb8cc684b6a39274971e1db2a81b21371ae12
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-19 16:48:47 +0900

    feat: switch to uefi

--- hosts/dayo/default.nix
+++ hosts/dayo/default.nix
@@ -1,18 +1,23 @@
 {
+  lib,
   pkgs,
-  inputs,
   ...
 }:
 {
   imports = [
     ./hardware-configuration.nix
-    inputs.nixos-hardware.nixosModules.raspberry-pi-4
+    # inputs.nixos-hardware.nixosModules.raspberry-pi-4
 
     # Server containers
     ../../modules/system/server/containers/home-assistant.nix
     ../../modules/system/server/gatus.nix
     ../../modules/system/server/homepage-dashboard.nix
   ];
+
+  boot.loader = {
+    systemd-boot.enable = lib.mkForce true;
+    generic-extlinux-compatible.enable = lib.mkForce false;
+  };
 
   # === Locale ===
   time.timeZone = "Asia/Tokyo";
