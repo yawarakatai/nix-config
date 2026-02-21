db19eb7df8795ca275ca40845926bf05fa15f26e
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-21 13:54:00 +0900

    feat: add vicinae cache to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   nixConfig = {
     extra-substituters = [
       "https://niri.cachix.org"
+      "https://vicinae.cachix.org"
     ];
     extra-trusted-public-keys = [
       "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
     ];
   };
 
