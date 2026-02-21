5fe1198a01cb1787e6a803de5682fcf628d59d26
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-02-21 11:30:00 +0900

    feat: update gaming/default.nix

--- modules/system/gaming/default.nix
+++ modules/system/gaming/default.nix
@@ -5,6 +5,13 @@
     ./steam.nix
   ];
 
+  # Launch option
+  # gamescope -W 1920 -H 1080 -r 60 -S integer -- %command%
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
   environment.systemPackages = with pkgs; [
     protonplus
     heroic
