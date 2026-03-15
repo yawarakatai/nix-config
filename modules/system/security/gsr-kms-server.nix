b78204f6490bc0bfd075790dd389237bbcb98607
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-03-16 08:08:26 +0900

    feat: add gsr-kms-server.nix

---
+++ modules/system/security/gsr-kms-server.nix
@@ -0,0 +1,10 @@
+{ pkgs, ... }:
+
+{
+  security.wrappers.gsr-kms-server = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_sys_admin+ep";
+    source = "${pkgs.gpu-screen-recorder}/bin/gsr-kms-server";
+  };
+}
