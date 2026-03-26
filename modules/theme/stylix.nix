f9fc3fe80ab513be84940f730674c8802d94e9df
yawarakatai <186454332+yawarakatai@users.noreply.github.com>
2026-03-26 13:32:58 +0900

    refact: update stylix.nix

--- modules/theme/stylix.nix
+++ modules/theme/stylix.nix
@@ -7,35 +7,6 @@ let
     url = "https://cdnb.artstation.com/p/assets/images/images/024/049/327/large/rodion-yushmanov-dsc-0198.jpg?1581157890";
     hash = "sha256-xJyvFH4zyHApiOjYEtgVPSeXz+ghuAIHs1fH8qCy8Z4=";
   };
-
-  processedWallpaper =
-    pkgs.runCommand "processed-wallpaper.png"
-      {
-        buildInputs = [ pkgs.imagemagick ];
-      }
-      ''
-        magick ${wallpaper} \
-        -blur 0x10 \
-        -brightness-contrast -10x0 \
-        $out
-      '';
-
-  miku-cursor = pkgs.stdenv.mkDerivation {
-    pname = "miku-cursor-linux";
-    version = "1.2.6";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "supermariofps";
-      repo = "hatsune-miku-windows-linux-cursors";
-      rev = "471ff88156e9a3dc8542d23e8cae4e1c9de6e732";
-      sha256 = "sha256-HCHo4GwWLvjjnKWNiHb156Z+NQqliqLX1T1qNxMEMfE=";
-    };
-
-    installPhase = ''
-      mkdir -p $out/share/icons
-      cp -r miku-cursor-linux $out/share/icons/
-    '';
-  };
 in
 {
   stylix = {
@@ -82,16 +53,10 @@ in
 
     # Cursor configuration
     cursor = {
-      package = pkgs.graphite-cursors;
-      name = "graphite-dark";
+      package = pkgs.phinger-cursors;
+      name = "phinger-cursors-dark";
       size = 32;
     };
-
-    # cursor = {
-    #   package = miku-cursor;
-    #   name = "miku-cursor-linux";
-    #   size = 32;
-    # };
 
     icons = {
       enable = true;
