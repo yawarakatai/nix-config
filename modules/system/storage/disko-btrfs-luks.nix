70f0ae6d150bc77d7282fc96e735b53919f0c46e
yawarakatai <me@yawarakatai.com>
2026-04-05 21:43:38 +0900

    feat: create disko-btrfs-luks.nix

---
+++ modules/system/storage/disko-btrfs-luks.nix
@@ -0,0 +1,75 @@
+{
+  device,
+  luksName ? "cryptroot",
+}:
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      inherit device;
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 1;
+            name = "ESP";
+            start = "1M";
+            end = "1G";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [
+                "fmask=0077"
+                "dmask=0077"
+              ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = luksName;
+              extraOpenArgs = [ "--allow-discards" ];
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
