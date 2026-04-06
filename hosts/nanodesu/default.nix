4681009621d12df78def40fe16789237dcdfcdba
yawarakatai <me@yawarakatai.com>
2026-04-06 22:40:03 +0900

    fix: efi font error

--- hosts/nanodesu/default.nix
+++ hosts/nanodesu/default.nix
@@ -18,29 +18,36 @@
     sbctl
   ];
 
-  boot.lanzaboote = {
+  boot = {
+    loader.systemd-boot.enable = lib.mkForce false;
+    lanzaboote = {
+      enable = true;
+      pkiBundle = "/etc/secureboot";
+    };
+
+    initrd = {
+      systemd = {
+        enable = true;
+        contents."/etc/vconsole.conf".text = lib.mkForce "FONT=\n";
+      };
+      luks.devices."cryptoroot" = {
+        crypttabExtraOpts = [
+          "tmp2-device=auto"
+          "tmp2-pcrs=7"
+        ];
+      };
+    };
+  };
+
+  security.tpm2 = {
     enable = true;
-    pkiBundle = "/etc/secureboot";
+    pkcs11.enable = true;
   };
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-
-  boot.initrd.systemd.enable = true;
-  boot.initrd.luks.devices."cryptoroot" = {
-    crypttabExtraOpts = [
-      "tmp2-device=auto"
-      "tmp2-pcrs=7"
-    ];
-  };
-
-  security.tpm2.enable = true;
-  security.tpm2.pkcs11.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # Workaround for Alder Lake audio firmware signature verification failure
-  boot.kernelParams = [
-    "snd_intel_dspcfg.dsp_driver=1"
-  ];
+  boot.kernelParams = [ "snd_intel_dspcfg.dsp_driver=1" ];
 
   # --- Application and Service Settings ---;
   services.logind.settings.Login = {
