404f2f2216eea6fcbc5afec905b7538a5bf892b0
yawarakatai <me@yawarakatai.com>
2026-04-07 14:43:49 +0900

    feat: add new layer for arrow keys

--- modules/system/input/keyboard/kanata.nix
+++ modules/system/input/keyboard/kanata.nix
@@ -12,9 +12,7 @@
           ;;
           ;;  Base: QWERTY physical -> Colemak DH output
           ;;  CapsLock: Tap for Esc, Hold for Ctrl
-          ;;  Enter:    Tap for Enter, Hold for Ctrl
           ;;  Space:    Tap for Space, Hold for Shift
-          ;;  RAlt: Symbols layer
           ;; =================================================================
 
           (defsrc
@@ -27,26 +25,25 @@
 
           (defalias
             escctl (tap-hold 150 150 esc lctl)
-            entctl (tap-hold 200 200 ret lctl)
             spcsft (tap-hold 200 200 spc lsft)
-            sym    (layer-while-held symbols)
+            bspcly (tap-hold 200 200 bspc (layer-toggle layer))
+            super  (tap-hold 200 200 M-tab lmet)
           )
 
           (deflayer base
             grv     1    2    3    4    5    6    7    8    9    0    -    =    bspc
             tab     q    w    f    p    b    j    l    u    y    ;    [    ]    \
-            @escctl a    r    s    t    g    m    n    e    i    o    '    @entctl
-            lsft    z    x    c    d    v    k    h    ,    .    /    rsft
-            lctl    lmet lalt           @spcsft        @sym rmet rctl
+            @escctl a    r    s    t    g    m    n    e    i    o    '    ret
+            z       x    c    d    v    j    k    h    ,    .    /    del
+            _       @super lalt         @spcsft        @bspcly   _    _
           )
 
-
-          (deflayer symbols
-            _    _    _    _    _    _    _    _    _    _    _    _    _    _
-            _    grv  S-,  S-.  S-2  S-5  _    _    _    _    _    _    _    _
-            _    S-grv S-9  S-0  [    ]    _    _    _    _    _    _    _
-            _    _    S-[  S-]  S-\  S-\  _    _    _    _    _    _
-            _    _    _              _              _    _    _
+          (deflayer layer
+            _     _     _     _     _     _     _     _     _     _     _     _     _     _ 
+            _     _     7     8     9     _     _     _     _     _     _     _     _     _ 
+            _     _     4     5     6     _     _     left  down  up    right _     _ 
+            _     _     1     2     3     _     _     _     _     _     _     _ 
+            _     _     _              0     _     _     _     
           )
         '';
       };
