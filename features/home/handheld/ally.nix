c8ddd2ef2a00aa9dfd2c148948add5548e28eace
yawarakatai <me@yawarakatai.com>
2026-05-16 12:27:10 +0900

    refactor: extract Ally handheld settings from GNOME module
    - Create features/home/handheld/ally.nix with EasyEffects (Dolby IRS
      preset + autostart), antimicrox (gamepad→mouse mapping + autostart),
      and audio plugins (lsp-plugins, calf, zam-plugins, mda_lv2)
    - Move ally-speakers-preset.json from features/gnome/ to features/home/handheld/
    - Strip Ally-specific packages and xdg.configFile from features/gnome/home.nix
    - Import handheld/ally.nix from hosts/kamo/home/default.nix
    
    nix flake check: PASSED

---
+++ features/home/handheld/ally.nix
@@ -0,0 +1,42 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    easyeffects
+    lsp-plugins
+    calf
+    zam-plugins
+    mda_lv2
+    antimicrox
+  ];
+
+  xdg.configFile =
+    let
+      dolbyIrs = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/JackHack96/EasyEffects-Presets/master/irs/Dolby%20ATMOS%20((128K%20MP3))%201.Default.irs";
+        name = "dolby-atmos.irs";
+        sha256 = "f45b751d92c54c188645f87fc0988667d5acb4cb6f76d5febb79556372425403";
+      };
+      presetJson = builtins.readFile ./ally-speakers-preset.json;
+    in
+    {
+      "easyeffects/irs/Dolby ATMOS ((128K MP3)) 1.Default.irs".source = dolbyIrs;
+      "easyeffects/output/ROG Ally Speakers.json".text = presetJson;
+      "autostart/easyeffects.desktop".text = ''
+        [Desktop Entry]
+        Name=EasyEffects
+        Exec=easyeffects --gapplication-service
+        Terminal=false
+        Type=Application
+        X-GNOME-Autostart-enabled=true
+      '';
+      "autostart/antimicrox.desktop".text = ''
+        [Desktop Entry]
+        Name=AntiMicroX
+        Exec=antimicrox --tray
+        Terminal=false
+        Type=Application
+        X-GNOME-Autostart-enabled=true
+      '';
+    };
+}
--- hosts/kamo/home/default.nix
+++ hosts/kamo/home/default.nix
@@ -5,5 +5,6 @@
     ../../../features/home/profiles/desktop-gnome.nix
     ../../../features/home/browser/zen-browser.nix
     ../../../features/home/terminal/ghostty.nix
+    ../../../features/home/handheld/ally.nix
   ];
 }
