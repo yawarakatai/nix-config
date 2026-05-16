888e316ec300192a002dfcc95a8f36339297a2aa
yawarakatai <me@yawarakatai.com>
2026-05-16 09:56:21 +0900

    fix: remove deprecated core-utilities option, update madori lock
    - Remove services.gnome.core-utilities.enable (renamed to core-apps.enable)
    - Update madori flake lock (PATH conflict resolved upstream)

--- features/gnome/system.nix
+++ features/gnome/system.nix
@@ -4,7 +4,6 @@
   services.desktopManager.gnome.enable = true;
 
   services.gnome.core-apps.enable = false;
-  services.gnome.core-utilities.enable = false;
   services.gnome.core-os-services.enable = true;
 
   services.gnome.rygel.enable = false;
