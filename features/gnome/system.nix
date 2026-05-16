ada2c958d93cfb89f2124969e6adedb2d2e7fb25
yawarakatai <me@yawarakatai.com>
2026-05-16 09:46:40 +0900

    fix(kamo): simplify gnome app exclusion, update syncthing device id
    - Simplify GNOME app exclusion: disable core-apps entirely instead of
      listing 17 individual packages. Keep core-os-services for essential
      daemons (gsd-power, gsd-media-keys, etc.). Only exclude gnome-tour.
    - Update kamo syncthing device ID to the new installation's ID
    - Update flake.lock

--- features/gnome/system.nix
+++ features/gnome/system.nix
@@ -3,28 +3,14 @@
 {
   services.desktopManager.gnome.enable = true;
 
-  services.gnome.core-apps.enable = true;
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-utilities.enable = false;
   services.gnome.core-os-services.enable = true;
 
   services.gnome.rygel.enable = false;
   services.gnome.gnome-online-accounts.enable = false;
 
   environment.gnome.excludePackages = with pkgs; [
-    baobab
-    cheese
-    epiphany
-    gnome-calendar
-    gnome-characters
-    gnome-clocks
-    gnome-connections
-    gnome-contacts
-    gnome-maps
-    gnome-music
-    gnome-software
-    gnome-weather
-    simple-scan
-    snapshot
-    totem
-    yelp
+    gnome-tour
   ];
 }
