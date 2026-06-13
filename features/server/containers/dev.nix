ae7b4566bd228b4957b032050a58c34a88d685cc
yawarakatai <me@yawarakatai.com>
2026-06-14 00:09:03 +0900

    feat: add dev container and private net

--- features/server/containers/dev.nix
+++ features/server/containers/dev.nix
@@ -2,14 +2,19 @@
 
 {
   containers.dev = {
-    autoStart = false;
-    ephemeral = true;
+    autoStart = true;
+    ephemeral = false;
+    privateNetwork = true;
+    hostAddress = "10.0.1.1";
+    localAddress = "10.0.1.2";
     bindMounts."/data/projects".hostPath = "/data/projects";
     config = { pkgs, ... }: {
       services.openssh.enable = true;
+      networking.firewall.enable = false;
       environment.systemPackages = with pkgs; [
         git
         neovim
+        htop
       ];
     };
   };
