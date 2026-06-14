29659f0532d90153e44e4e60f883ec2b279db31e
yawarakatai <me@yawarakatai.com>
2026-06-14 13:09:53 +0900

    fix(caddy): use correct cloudflare DNS plugin version v0.2.4

--- features/server/caddy.nix
+++ features/server/caddy.nix
@@ -19,7 +19,7 @@
   services.caddy = {
     enable = true;
     package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@v1.0.0" ];
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
       hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
     };
 
