086d8544165cad86dddc20ac36a5663533ca9a07
yawarakatai <me@yawarakatai.com>
2026-07-08 06:57:52 +0900

    refactor: separate ui option from theme

--- modules/theme/default.nix
+++ modules/theme/default.nix
@@ -1,5 +1,8 @@
 { lib, ... }:
-with lib;
+
+let
+  inherit (lib) mkOption types;
+in
 {
   imports = [
     ./stylix.nix
@@ -10,70 +13,57 @@ with lib;
       type = types.int;
       default = 10;
     };
+
     gaps = mkOption {
       type = types.int;
       default = 14;
     };
-    bar = {
-      position = mkOption {
-        type = types.enum [
-          "top"
-          "bottom"
-          "left"
-          "right"
-        ];
-        default = "bottom";
-      };
-      marginEnds = mkOption {
-        type = types.int;
-        default = 1280;
-      };
-      thickness = mkOption {
-        type = types.int;
-        default = 56;
-      };
-      padding = mkOption {
-        type = types.int;
-        default = 12;
-      };
-    };
+
     terminal = {
       padding = mkOption {
         type = types.int;
         default = 20;
       };
     };
+
     opacity = {
       terminal = mkOption {
         type = types.float;
         default = 0.85;
       };
+
       applications = mkOption {
         type = types.float;
         default = 1.0;
       };
+
       desktop = mkOption {
         type = types.float;
         default = 1.0;
       };
+
       popups = mkOption {
         type = types.float;
         default = 0.90;
       };
+
       shell = mkOption {
         type = types.float;
         default = 0.65;
       };
+
       shellPopups = mkOption {
         type = types.float;
         default = 0.78;
       };
     };
+
     border = {
       enable = mkOption {
         type = types.bool;
         default = false;
       };
+
       width = mkOption {
         type = types.int;
         default = 0;
