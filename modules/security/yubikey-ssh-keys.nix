63d4e2c0266926503db4d20ebba2e4ed9804e661
yawarakatai <me@yawarakatai.com>
2026-08-07 13:36:32 +0900

    refactor: keep ssh secrets out of home

--- modules/security/yubikey-ssh-keys.nix
+++ modules/security/yubikey-ssh-keys.nix
@@ -2,7 +2,6 @@
 {
   age.secrets.yubikey-5-sk = {
     rekeyFile = ../../secrets/yubikey-5-sk.age;
-    path = "/home/${username}/.ssh/yubikey_5";
     owner = username;
     group = "users";
     mode = "600";
@@ -10,7 +9,6 @@
 
   age.secrets.yubikey-5c-sk = {
     rekeyFile = ../../secrets/yubikey-5c-sk.age;
-    path = "/home/${username}/.ssh/yubikey_5c";
     owner = username;
     group = "users";
     mode = "600";
