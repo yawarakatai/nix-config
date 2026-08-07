dd3fa092902400c4057a0df6f560a7887621b324
yawarakatai <me@yawarakatai.com>
2026-08-07 13:52:15 +0900

    fix: nest ssh client config under config

--- modules/home/services/ssh-client.nix
+++ modules/home/services/ssh-client.nix
@@ -17,50 +17,52 @@ in
     description = "SSH identity files used for hosts managed by this configuration.";
   };
 
-  home.file = {
-    ".ssh/yubikey_5.pub".text = keys.ssh.yubikey-5;
-    ".ssh/yubikey_5c.pub".text = keys.ssh.yubikey-5c;
-  };
+  config = {
+    home.file = {
+      ".ssh/yubikey_5.pub".text = keys.ssh.yubikey-5;
+      ".ssh/yubikey_5c.pub".text = keys.ssh.yubikey-5c;
+    };
 
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
 
-    settings = {
-      "*".setEnv = "TERM=xterm-256color";
+      settings = {
+        "*".setEnv = "TERM=xterm-256color";
 
-      "desuwa" = {
-        hostname = "desuwa.yawarakatai.com";
-        identityFile = identities;
-        identitiesOnly = true;
-        identityAgent = "none";
-      };
+        "desuwa" = {
+          hostname = "desuwa.yawarakatai.com";
+          identityFile = identities;
+          identitiesOnly = true;
+          identityAgent = "none";
+        };
 
-      "dane" = {
-        hostname = "dane.yawarakatai.com";
-        identityFile = identities;
-        identitiesOnly = true;
-        identityAgent = "none";
-      };
+        "dane" = {
+          hostname = "dane.yawarakatai.com";
+          identityFile = identities;
+          identitiesOnly = true;
+          identityAgent = "none";
+        };
 
-      "kamo" = {
-        hostname = "kamo.yawarakatai.com";
-        identityFile = identities;
-        identitiesOnly = true;
-        identityAgent = "none";
-      };
+        "kamo" = {
+          hostname = "kamo.yawarakatai.com";
+          identityFile = identities;
+          identitiesOnly = true;
+          identityAgent = "none";
+        };
 
-      "github.com" = {
-        user = "git";
-        identityFile = identities;
-        identitiesOnly = true;
-        identityAgent = "none";
-      };
+        "github.com" = {
+          user = "git";
+          identityFile = identities;
+          identitiesOnly = true;
+          identityAgent = "none";
+        };
 
-      "192.168.*" = {
-        identityFile = identities;
-        identitiesOnly = true;
-        identityAgent = "none";
+        "192.168.*" = {
+          identityFile = identities;
+          identitiesOnly = true;
+          identityAgent = "none";
+        };
       };
     };
   };
