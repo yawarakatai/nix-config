c0b1a04ce6d82dadc9345b858912e0a526ccade6
yawarakatai <me@yawarakatai.com>
2026-08-30 09:38:15 +0900

    feat(kamo): add vesktop

--- hosts/kamo/home/default.nix
+++ hosts/kamo/home/default.nix
@@ -10,6 +10,7 @@
     self.modules.homeManager.profiles.desktopNiri
     ../../../modules/home/services/ura.nix
     ../../../modules/home/dev/herdr.nix
+    ../../../modules/home/communication
   ];
 
   home.packages = with pkgs; [
