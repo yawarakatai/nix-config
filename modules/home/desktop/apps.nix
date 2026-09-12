38d2be418580e8d9b25800828b4a2f41e12078bd
yawarakatai <me@yawarakatai.com>
2026-09-12 13:34:04 +0900

    feat: add nautilus with unar

--- modules/home/desktop/apps.nix
+++ modules/home/desktop/apps.nix
@@ -3,9 +3,23 @@
 {
   home.packages = with pkgs; [
     wlr-randr
+    nautilus
     swayimg
     mpv
     playerctl
     brightnessctl
+    unar
   ];
+
+  # Nautilus' built-in gnome-autoar extractor has no setting for legacy
+  # archive filename encodings.  Expose unar in the Scripts context menu.
+  home.file.".local/share/nautilus/scripts/Extract with unar" = {
+    source = pkgs.writeShellScript "nautilus-extract-with-unar" ''
+      set -eu
+
+      for archive do
+        unar -o "$(dirname -- "$archive")" "$archive"
+      done
+    '';
+  };
 }
